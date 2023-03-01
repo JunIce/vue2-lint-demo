969187f2a174bcc0946c179a49e341b3c40b1ecc
JunIce <east.qrj@gmail.com>
2023-03-01 10:30:38 +0800

    chore(): add composition-api deps

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vueCompositionApi, { createApp } from "@vue/composition-api";
 
 Vue.config.productionTip = false;
+Vue.use(vueCompositionApi);
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+createApp(App).mount("#app");
