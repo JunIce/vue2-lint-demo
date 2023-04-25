8008e39656d8de8446b84b76a6b42cae27c38a17
JunIce <east.qrj@gmail.com>
2023-04-25 15:25:30 +0800

    stage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <KonvaDemo></KonvaDemo>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    KonvaDemo: () => import("./components/konva.vue"),
   },
 };
 </script>
@@ -23,6 +25,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/konva.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div id="container"></div>
+    <textarea id="textarea"></textarea>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "@vue/composition-api";
+import Konva from "konva";
+export default {
+  name: "KonvaDemo",
+  setup() {
+    onMounted(() => {
+      const textarea = document.querySelector("#textarea");
+      var stage = new Konva.Stage({
+        container: "container",
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+
+      // add canvas element
+      var layer = new Konva.Layer();
+      stage.add(layer);
+
+      // create shape
+      // var box = new Konva.Rect({
+      //   x: 50,
+      //   y: 50,
+      //   width: 100,
+      //   height: 50,
+      //   fill: "#00D2FF",
+      //   stroke: "black",
+      //   strokeWidth: 4,
+      //   draggable: true,
+      // });
+      // layer.add(box);
+
+      // var circle = new Konva.Circle({
+      //   x: 200,
+      //   y: 200,
+      //   radius: 70,
+      //   fill: "red",
+      //   stroke: "black",
+      //   strokeWidth: 4,
+      //   draggable: true,
+      // });
+      // layer.add(circle);
+
+      // var complexText = new Konva.Text({
+      //   x: 30,
+      //   y: 20,
+      //   text: "COMPLEX TEXT\n\nAll the world's a stage, and all the men and women merely players. They have their exits and their entrances.",
+      //   fontSize: 18,
+      //   fontFamily: "Calibri",
+      //   fill: "#555",
+      //   width: 300,
+      //   padding: 20,
+      //   align: "center",
+      // });
+
+      // layer.add(complexText);
+
+      // // add cursor styling
+      // box.on("mouseover", function () {
+      //   document.body.style.cursor = "pointer";
+      // });
+      // box.on("mouseout", function () {
+      //   document.body.style.cursor = "default";
+      // });
+
+      // layer.on("click", function (e) {
+      //   console.log("you clicked/touched me!");
+      //   console.log(e);
+      // });
+
+      const textNode = new Konva.Text({
+        x: 20,
+        y: 20,
+        width: window.innerWidth - 40,
+        fontSize: 30,
+        fontFamily: "Calibri",
+        fill: "green",
+      });
+
+      layer.add(textNode);
+
+      textarea.addEventListener("input", (e) => {
+        console.log(e.target.value);
+        textNode.text(e.target.value);
+      });
+    });
+  },
+};
+</script>
+
+<style scoped>
+textarea {
+  position: absolute;
+  top: 200px;
+  left: 100px;
+  width: 100px;
+}
+</style>
